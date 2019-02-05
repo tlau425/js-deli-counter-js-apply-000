f139fd4178a69ca780e75b3a541097480295e17f
Tommy Lau <tlau425@yahoo.com>
2019-02-05 16:38:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function takeANumber(arr, name){
+function takeANumber(arr){
   arr.push(name);
 return ('Welcome, ' + name + '. You are number ' + (arr.indexOf(name)+1) + ' in line.');
 
@@ -26,3 +26,4 @@ function currentLine(line){
   } else
   return ('The line is currently:' + newArr);
 }
+
